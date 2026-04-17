44841f6fca02e8053dc9eabc46b7d053e4c13960
DUWIPANGGA <aryad2232@gmail.com>
2026-04-17 13:02:53 +0700

    ifran

--- resources/views/pages/dashboard/user.blade.php
+++ resources/views/pages/dashboard/user.blade.php
@@ -1,16 +1,4 @@
 @extends('layout.app')
 @section('main')
-    <div class="absolute top-1/3 left-4">
-        <x-icons.shirt_1 class="" />
-    </div>
-    <div class="absolute top-1/3 right-0">
-        <x-icons.shirt_2 class="" />
-    </div>
-    <div class="h-screen flex flex-col justify-center items-center align-center">
-        <h1 class="h1 text-center">Jersey Custom Berkualitas</h1>
-        <div class="h3">Dari tim komunitas sampai klub kompetitif, kami produksi jersey yang sesuai identitas tim Anda.
-            Bahan
-            tahan lama, cetak presisi, dan kualitas konsisten di setiap produksi.</div>
-        <x-ui.button>Mulai</x-ui.button>
-    </div>
+
 @endsection
