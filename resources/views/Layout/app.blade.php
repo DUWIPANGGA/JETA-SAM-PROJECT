a2f75db16861f86c1c4bca4e819e10c9f0bd4df9
Ifran <mohammadifran22@gmail.com>
2026-04-17 11:18:45 +0700

    progress user dashboard

--- resources/views/Layout/app.blade.php
+++ resources/views/Layout/app.blade.php
@@ -6,11 +6,12 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <script src="https://cdn.tailwindcss.com"></script>
-    <title>Document</title>
+    <title></title>
 </head>
 <body>
     <div class="bg-[url('/images/bg.png')] w-full min-h-screen bg-repeat bg-[length:50px_50px]">
         @include('layout.partials.navbar')
+        @yield('user_dashboard')
     </div>
 </body>
 </html>
--- resources/views/pages/dashboard/user.blade.php
+++ resources/views/pages/dashboard/user.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <<meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -11,7 +11,7 @@
 <body>
     @include('layout.app')
     <ul>
-        <li><x-icons.shirt_1/></li>
+        <li><x-icons.shirt_1 class="flex "/></li>
         <li>
             <ul>
                 <li></li>
