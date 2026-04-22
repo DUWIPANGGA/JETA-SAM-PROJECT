964eeae70338ebff732c02fede7e25a56725a2b1
Ifran <mohammadifran22@gmail.com>
2026-04-22 14:31:55 +0700

    update landing page

--- resources/views/components/ui/button.blade.php
+++ resources/views/components/ui/button.blade.php
@@ -1,3 +1,14 @@
-<button class="button-primary">
+
+<button type="submit" class="flex items-center justify-center text-white px-8 py-1 gap-2 w-[205px] h-[50px] bg-[#2579F6] rounded-full flex-none shadow-[0px_4px_10px_rgba(31,40,50,0.2),0px_2px_4px_rgba(31,40,50,0.2),inset_0px_-2px_2px_rgba(83,83,83,0.4),inset_0px_2px_2px_rgba(255,255,255,0.5)]">
     {{ $slot }}
 </button>
+
+{{-- <button type="submit"
+                    class="bg-[#2563eb] hover:bg-blue-700 text-white rounded-full px-6 py-2.5 text-sm font-medium flex items-center justify-center mb-8 shadow-sm shadow-blue-500/50">
+                    Login
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5"
+                        stroke="currentColor" class="w-4 h-4 ml-1">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
+                    </svg>
+                </button> --}}
